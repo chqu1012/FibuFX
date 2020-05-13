f4d87acef8b405fccc90b59bd0b26a82623f4479
chauq <chauq@LAPTOP-0387JN3P>
2020-05-13 17:17:53 +0200

    FIBU-00029: Showing Einnahmen Ausgaben vorlagen

--- de.dc.fibufx.client14/src/main/java/de/dc/fibufx/client/controller/BaseKontoverwaltungController.java
+++ de.dc.fibufx.client14/src/main/java/de/dc/fibufx/client/controller/BaseKontoverwaltungController.java
@@ -1,11 +1,14 @@
 package de.dc.fibufx.client.controller;
+import de.dc.fibufx.client.model.Buchungsvorgang;
 import de.dc.fibufx.client.model.Konto;
 import de.dc.fibufx.client.model.KontoTyp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 
 public abstract class BaseKontoverwaltungController {
@@ -23,9 +26,53 @@ public abstract class BaseKontoverwaltungController {
     protected Button buttonKontoErstellen;
 
     @FXML
+    protected Button buttonBuchungsvorgangErstellen;
+
+    @FXML
     protected ListView<Konto> listViewKonto;
 
     @FXML
-    protected abstract void onButtonAction(ActionEvent event);
+    protected TextField textSearchEinnahmen;
 
+    @FXML
+    protected ListView<Buchungsvorgang> listViewEinnnahmen;
+
+    @FXML
+    protected MenuItem menuItemBuchungstypeLoeschen;
+
+    @FXML
+    protected TextField textSearchAusgaben;
+
+    @FXML
+    protected ListView<Buchungsvorgang> listViewAusgaben;
+
+    @FXML
+    protected MenuItem menuItemBuchungstypeLoeschen1;
+
+    @FXML
+    protected Label labelEinnahmenCount;
+
+    @FXML
+    protected Label labelAusgabenCount;
+
+    @FXML
+    protected ComboBox<String> comboBuchungstype;
+
+    @FXML
+    protected TextField textBuchungstypeName;
+
+    @FXML
+    protected TextField textBuchungstypeAnschrift;
+
+    @FXML
+    protected TextField textBuchungstypeMobilnummer;
+
+    @FXML
+    protected TextField textBuchungstypeIdentification;
+
+    @FXML
+    protected TextField textBuchungstypeNotiz;
+
+    @FXML
+    protected abstract void onButtonAction(ActionEvent event);
 }
--- de.dc.fibufx.client14/src/main/java/de/dc/fibufx/client/controller/KontoverwaltungController.java
+++ de.dc.fibufx.client14/src/main/java/de/dc/fibufx/client/controller/KontoverwaltungController.java
@@ -1,6 +1,7 @@
 package de.dc.fibufx.client.controller;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
@@ -8,49 +9,120 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
 
 import de.dc.fibufx.client.controller.cell.BankListCell;
+import de.dc.fibufx.client.model.Buchungstype;
+import de.dc.fibufx.client.model.Buchungsvorgang;
 import de.dc.fibufx.client.model.Konto;
 import de.dc.fibufx.client.model.KontoTyp;
 import de.dc.fibufx.client.service.StammdatenService;
+import javafx.beans.binding.Bindings;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 
 @Controller
 public class KontoverwaltungController extends BaseKontoverwaltungController {
-	
-	@Autowired StammdatenService stammdatenService;
-	@Autowired RestTemplate restTemplate;
-	
+
+	@Autowired
+	StammdatenService stammdatenService;
+	@Autowired
+	RestTemplate restTemplate;
+
 	private ObservableList<Konto> bankList = FXCollections.observableArrayList();
+
+	private FilteredList<Buchungsvorgang> filteredEinnahmen;
+	private FilteredList<Buchungsvorgang> filteredAusgaben;
 
 	public void initialize() {
 		listViewKonto.setCellFactory(e -> new BankListCell());
 		listViewKonto.setItems(bankList);
-		
+
 		bankList.addAll(stammdatenService.getKonten());
+
+		ObservableList<Buchungsvorgang> dataAusgaben = stammdatenService.getAusgabenTypen();
+		ObservableList<Buchungsvorgang> dataEinnahmen = stammdatenService.getEinnahmenTypen();
+
+		filteredEinnahmen = new FilteredList<>(dataEinnahmen);
+		filteredAusgaben = new FilteredList<>(dataAusgaben);
+
+		listViewAusgaben.setItems(filteredAusgaben);
+		listViewEinnnahmen.setItems(filteredEinnahmen);
+
+		labelAusgabenCount.textProperty().bind(Bindings.size(dataAusgaben).asString());
+		labelEinnahmenCount.textProperty().bind(Bindings.size(dataEinnahmen).asString());
+
+		comboBuchungstype.setItems(FXCollections.observableArrayList("Einnahmen", "Ausgaben"));
+		comboBuchungstype.getSelectionModel().selectFirst();
+		
 		comboKasse.setItems(FXCollections.observableArrayList(KontoTyp.values()));
 		comboKasse.getSelectionModel().selectFirst();
+
+		textSearchAusgaben.textProperty().addListener(this::filterAusgaben);
+		textSearchEinnahmen.textProperty().addListener(this::filterEinnahmen);
 	}
-	
+
+	private void filterAusgaben(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+		if (newValue != null) {
+			filteredAusgaben.setPredicate(p -> {
+				boolean isEmpty = p.getName() == null;
+				boolean containName = p.getName().toLowerCase().contains(newValue.toLowerCase());
+				return isEmpty || containName;
+			});
+		}
+	}
+
+	private void filterEinnahmen(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+		if (newValue != null) {
+			filteredEinnahmen.setPredicate(p -> {
+				boolean isEmpty = p.getName() == null;
+				boolean containName = p.getName().toLowerCase().contains(newValue.toLowerCase());
+				return isEmpty || containName;
+			});
+		}
+	}
+
 	@Override
 	protected void onButtonAction(ActionEvent event) {
 		Object source = event.getSource();
 		if (source == buttonKontoErstellen) {
-			Konto konto = new Konto();
-			konto.setAktualisiert(LocalDate.now());
-			konto.setBestand(Double.parseDouble(textBestad.getText()));
-			konto.setBezeichnung(textKontoName.getText());
-			konto.setErstellt(LocalDate.now());
-			konto.setTyp(comboKasse.getSelectionModel().getSelectedItem());
-			
-			HttpEntity<Konto> request = new HttpEntity<>(konto);
-			konto = restTemplate.postForObject("http://localhost:2001/createKonto", request, Konto.class);
-			
-			bankList.add(0, konto);
-			
-			textBestad.setText("");
-			textKontoName.setText("");
+			dispatchOnCreateKonto();
+		} else if (source == buttonBuchungsvorgangErstellen) {
+			dispatchOnCreateVorgang();
 		}
+	}
+
+	private void dispatchOnCreateVorgang() {
+		boolean isEinnahmen =comboBuchungstype.getSelectionModel().getSelectedIndex()==0;
+		Buchungsvorgang vorgang = new Buchungsvorgang();
+		vorgang.setErstelltAm(LocalDateTime.now());
+		vorgang.setName(textBuchungstypeName.getText());
+		if (isEinnahmen) {
+			vorgang.setTyp(Buchungstype.EINNAHME);
+			stammdatenService.getEinnahmenTypen().add(vorgang);
+		}else {
+			vorgang.setTyp(Buchungstype.AUSGABE);
+			stammdatenService.getAusgabenTypen().add(vorgang);
+			
+		}
+	}
+
+	private void dispatchOnCreateKonto() {
+		Konto konto = new Konto();
+		konto.setAktualisiert(LocalDate.now());
+		konto.setBestand(Double.parseDouble(textBestad.getText()));
+		konto.setBezeichnung(textKontoName.getText());
+		konto.setErstellt(LocalDate.now());
+		konto.setTyp(comboKasse.getSelectionModel().getSelectedItem());
+
+		HttpEntity<Konto> request = new HttpEntity<>(konto);
+		konto = restTemplate.postForObject("http://localhost:2001/createKonto", request, Konto.class);
+
+		bankList.add(0, konto);
+
+		textBestad.setText("");
+		textKontoName.setText("");
 	}
 
 }
